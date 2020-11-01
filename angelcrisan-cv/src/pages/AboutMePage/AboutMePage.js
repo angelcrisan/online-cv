0fcaf91ead575e8b4227ad9954c014306a8715d6
Your Name <angelcrisan@live.com>
2020-11-01 19:12:24 +0200

    new commit 2

--- angelcrisan-cv/src/pages/AboutMePage/AboutMePage.js
+++ angelcrisan-cv/src/pages/AboutMePage/AboutMePage.js
@@ -9,8 +9,13 @@ export class AboutMePage extends React.Component {
     render(){
         return(
             <div className="aboutme-section">
+            <div className="about-details">
+                <p>I am a very sociable person, interested in discovering everything that is new for me. I followed INTRO IN IT and FRONT-END WEB DEVELOPMENT courses at SCOALA INFORMALA DE IT, and my goal is to become one day a full-time front-end developer. Also, part time I am a professional basketball referee so to work in a team don't scares me. </p>
+            </div>
 
+            <div className="hobbies-section">
             <h2>Hobbies</h2>
+            <hr></hr>
             <p>Gaming</p>
             <img src={GamingLogo} alt="gaming-controller" className="gaming-logo"/>
             <p>Basketball (Part-time basketball referee)</p>
@@ -19,6 +24,7 @@ export class AboutMePage extends React.Component {
             <img src={FanLogo} alt="supporter" className="fan-logo"/>
             <p>TV shows</p>
             <img src={TvLogo} alt="tv-silouette" className="tv-logo"/>
+            </div>
             </div>
         )
     }
