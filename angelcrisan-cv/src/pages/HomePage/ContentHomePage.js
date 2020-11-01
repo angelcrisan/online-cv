e49fada62041a13ba0a4c8568534f097683805db
Your Name <angelcrisan@live.com>
2020-11-01 17:53:21 +0200

    new commit

--- angelcrisan-cv/src/pages/HomePage/ContentHomePage.js
+++ angelcrisan-cv/src/pages/HomePage/ContentHomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './HomePage.css';
-import ProfilePic from '../HomePage/profile-picture.jpg';
+import ProfilePic from '../HomePage/profile-pic.jpg';
 import './ContentHomePage.css';
 import FacebookLogo from '../HomePage/facebook-logo.svg';
 import InstagramLogo from '../HomePage/instagram-logo.svg';
@@ -13,17 +13,17 @@ export class ContentHomePage extends React.Component {
                 <h1>TUDOR-ANGEL CRISAN</h1>
                 <h3>24 YEARS OLD</h3>
                 <h3>ASPIRING TO BECOME A FRONT-END DEVELOPER</h3>
-                <img src={ProfilePic} alt="profile-picture" className="profile-picture"></img>
+                <img src={ProfilePic} alt="profile" className="profile-picture"></img>
 
                 <div className="socialMedia">
-                <a href="https://www.facebook.com/angeel.crisan" target="_blank">
-                <img src={FacebookLogo} alt="facebook" className="facebook-profile" className="socialProfiles"></img>
+                <a href="https://www.facebook.com/angeel.crisan" target="_blank" rel= "noopener noreferrer">
+                <img src={FacebookLogo} alt="facebook" className="socialProfiles"></img>
                 </a>
-                <a href="https://www.instagram.com/angelcrisan_/" target="_blank">
-                <img src={InstagramLogo} alt="instagram" className="instagram-profile" className="socialProfiles"></img>
+                <a href="https://www.instagram.com/angelcrisan_/" target="_blank" rel= "noopener noreferrer">
+                <img src={InstagramLogo} alt="instagram" className="socialProfiles"></img>
                 </a>
-                <a href="https://www.linkedin.com/in/angel-crisan-14296b1b9/" target="_blank">
-                <img src={LinkedinLogo} alt="linkedin" className="linkedin-profile" className="socialProfiles"></img>
+                <a href="https://www.linkedin.com/in/angel-crisan-14296b1b9/" target="_blank" rel= "noopener noreferrer">
+                <img src={LinkedinLogo} alt="linkedin" className="socialProfiles"></img>
                 </a>
                 </div>
             </div>
